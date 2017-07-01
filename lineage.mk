058be99fab97453130dad2174ac93a515f598bcb
Unknown <danielmania93@gmail.com>
2017-07-01 13:43:57 +0200

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -1,6 +1,6 @@
 PRODUCT_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-$(call inherit-product, device/lge/c50n/full_ms345.mk)
+$(call inherit-product, device/lge/c50n/full_c50n.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
@@ -9,6 +9,6 @@ PRODUCT_NAME := cm_c50n
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="c50n" \
-    PRODUCT_NAME="c50_mpcs_us" \
-    BUILD_FINGERPRINT="MetroPCS/c50_mpcs_us/c50:5.1.1/LMY47V/151671739a00f:user/release-keys" \
-    PRIVATE_BUILD_DESC="c50_mpcs_us-user 5.1.1 LMY47V 151671739a00f release-keys"
+    PRODUCT_NAME="c50n_global_com" \
+    BUILD_FINGERPRINT="lge/c50n_global_com/c50n:6.0/MRA58K/161761508b4d6:user/release-keys" \
+    PRIVATE_BUILD_DESC="c50n_global_com-user 6.0 MRA58K 161761508b4d6 release-keys"
