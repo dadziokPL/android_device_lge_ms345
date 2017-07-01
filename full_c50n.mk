b1d5661a30cc16396c88a7159165a0c0d6601c48
dadziokPL <danielmania93@gmail.com>
2017-07-01 15:15:13 +0200

    Update full_c50n.mk

--- full_c50n.mk
+++ full_c50n.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from ms345 device
-$(call inherit-product, device/lge/ms345/device.mk)
+$(call inherit-product, device/lge/c50n/device.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := c50n
